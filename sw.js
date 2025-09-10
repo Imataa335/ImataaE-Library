13892d2822a92b8b069daf53680529777c03eba1
Imataa <ephraimimataa@gmail.com>
2025-09-10 12:30:59 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+
+const cacheName = 'imataa-elibrary-v1';
+const assets = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png'
+];
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+});
